54ea82b6e5676cde70904da53d89c87778b1930e
thejus-r <thejusr1999@gmail.com>
2021-01-14 01:04:15 +0530

    Experiment 1 finished

---
+++ Experiment 1/exp1.sql
@@ -0,0 +1,41 @@
+USE cs333;
+
+-- Creating a table named "student"
+CREATE TABLE student
+(
+    sno INT NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    dept VARCHAR(25) NOT NULL,
+    mark INT NOT NULL,
+    PRIMARY KEY(sno)
+);
+
+-- Added a column "age" to student
+ALTER TABLE student
+ADD age INT;
+
+-- Inserting first row into TABLE
+INSERT INTO student
+    (sno,name,dept,mark,age)
+VALUES
+    (59, 'Thejus R', 'CS', 76, 21);
+
+-- Display all from TABLE
+SELECT *
+FROM student;
+
+-- Changing the datatype of a Column
+ALTER TABLE student ALTER COLUMN dept varchar(50);
+
+-- Dropping a column 
+ALTER TABLE student DROP COLUMN mark;
+
+-- Display all from TABLE
+SELECT *
+FROM student;
+
+-- Renaming the TABLE
+EXEC sp_rename 'student','students';
+
+-- Dropping a TABLE
+DROP TABLE students;
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Application Software Development Lab (cs333)
 
 ## CHN18CS118
+
+| SI  | Experiment Name | Submission Date    | Output File                                                                       |
+| --- | --------------- | ------------------ | --------------------------------------------------------------------------------- |
+| 1   | DEMO            | January 14th, 2021 | -nil-                                                                             |
+| 2   | Experiment 1    | January 14th, 2021 | [Output](https://github.com/ceccs18c59/cs331/blob/main/Experiment%202/report.pdf) |
